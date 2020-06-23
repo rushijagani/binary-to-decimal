ef4f9e35db7c33a9a8e2410a295927433602166a
rushi.jagani <rushi.jagani@gmail.com>
2020-06-23 14:57:33 +0530

    fit: converter added

---
+++ src/component/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from '../container/Header';
+import Converter from './Converter';
+import Footer from '../container/Footer';
+import './main.scss';
+
+function App() {
+  return (
+    <>
+    <Header />
+    <Converter />
+    <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/container/Header/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './header.scss';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="header__container">
+                <h2> Binary To Decimal  | Decimal To Binary</h2>
+                <span>Converter</span>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
