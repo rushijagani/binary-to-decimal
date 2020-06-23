2a67581e1c8b031c63460f44faa6a612bca49b4b
rushi.jagani <rushi.jagani@gmail.com>
2020-06-23 15:10:25 +0530

    Updates

---
+++ precache-manifest.7f5e9e7488124105c9776c896636d9f6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b700312d38ccc1ce9137dd777176ba5",
+    "url": "/binary-to-decimal/index.html"
+  },
+  {
+    "revision": "3635dfba5e936dbef897",
+    "url": "/binary-to-decimal/static/css/main.6baa816a.chunk.css"
+  },
+  {
+    "revision": "37c5695fbaf288b63f9c",
+    "url": "/binary-to-decimal/static/js/2.5aa52ecc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/binary-to-decimal/static/js/2.5aa52ecc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3635dfba5e936dbef897",
+    "url": "/binary-to-decimal/static/js/main.f4f39832.chunk.js"
+  },
+  {
+    "revision": "2c82df3ec38b156179f4",
+    "url": "/binary-to-decimal/static/js/runtime-main.44fa09f8.js"
+  },
+  {
+    "revision": "422e766543a3850de8db1f049775a1e9",
+    "url": "/binary-to-decimal/static/media/facebook.422e7665.svg"
+  },
+  {
+    "revision": "a112a001d5170d13d5f46d9387e526a4",
+    "url": "/binary-to-decimal/static/media/github.a112a001.svg"
+  },
+  {
+    "revision": "91a8e68dd3c119bb22f52aaff450c3b9",
+    "url": "/binary-to-decimal/static/media/instagram.91a8e68d.svg"
+  },
+  {
+    "revision": "89dcd6955d20dc3183df9d8ba408312c",
+    "url": "/binary-to-decimal/static/media/linkedin.89dcd695.svg"
+  },
+  {
+    "revision": "bfe8a6b50ba5128064b2294299fa8cc4",
+    "url": "/binary-to-decimal/static/media/mail.bfe8a6b5.svg"
+  },
+  {
+    "revision": "88c38bdc6f8a183f915ded830d225ef3",
+    "url": "/binary-to-decimal/static/media/twitter.88c38bdc.svg"
+  },
+  {
+    "revision": "4dab8248221d9891a3e5b2b30f435303",
+    "url": "/binary-to-decimal/static/media/wordpress.4dab8248.svg"
+  }
+]);
