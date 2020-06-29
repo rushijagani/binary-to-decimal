eff54da4ccb45849591f32c2a627937940d0da47
rushi.jagani <rushi.jagani@gmail.com>
2020-06-29 15:32:29 +0530

    Updates

--- precache-manifest.d601287ae3a5745fe428780883863b97.js
+++ precache-manifest.d601287ae3a5745fe428780883863b97.js
@@ -1,39 +1,31 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a1d477fbaa58ad493ad6fb441b0ef5ca",
+    "revision": "b0ffe76077c7cda506f32bcb32ea6ec4",
     "url": "/binary-to-decimal/index.html"
   },
   {
-    "revision": "d43a71f8ea57b46d7119",
+    "revision": "9deba38c5ec883000169",
     "url": "/binary-to-decimal/static/css/main.0473d909.chunk.css"
   },
   {
-    "revision": "37c5695fbaf288b63f9c",
-    "url": "/binary-to-decimal/static/js/2.5aa52ecc.chunk.js"
+    "revision": "6102326f0a9d1f5d22c7",
+    "url": "/binary-to-decimal/static/js/2.dc2df98b.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/binary-to-decimal/static/js/2.5aa52ecc.chunk.js.LICENSE.txt"
+    "url": "/binary-to-decimal/static/js/2.dc2df98b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d43a71f8ea57b46d7119",
-    "url": "/binary-to-decimal/static/js/main.1ead0fd4.chunk.js"
+    "revision": "9deba38c5ec883000169",
+    "url": "/binary-to-decimal/static/js/main.e404f996.chunk.js"
   },
   {
     "revision": "2c82df3ec38b156179f4",
     "url": "/binary-to-decimal/static/js/runtime-main.44fa09f8.js"
   },
   {
-    "revision": "422e766543a3850de8db1f049775a1e9",
-    "url": "/binary-to-decimal/static/media/facebook.422e7665.svg"
-  },
-  {
     "revision": "a112a001d5170d13d5f46d9387e526a4",
     "url": "/binary-to-decimal/static/media/github.a112a001.svg"
-  },
-  {
-    "revision": "91a8e68dd3c119bb22f52aaff450c3b9",
-    "url": "/binary-to-decimal/static/media/instagram.91a8e68d.svg"
   },
   {
     "revision": "89dcd6955d20dc3183df9d8ba408312c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/binary-to-decimal/precache-manifest.ad356bf58a8e9aaf9f279de0c3bd8035.js"
+  "/binary-to-decimal/precache-manifest.d601287ae3a5745fe428780883863b97.js"
 );
 
 self.addEventListener('message', (event) => {
